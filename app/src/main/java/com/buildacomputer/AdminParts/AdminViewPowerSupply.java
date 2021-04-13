64c6bda8aaa6eb50f6dfdd98458594dcd534e094
Tim <tjrodgers49@gmail.com>
2021-04-13 13:32:12 -0700

    Setup Java files for admin pages.

---
+++ app/src/main/java/com/buildacomputer/AdminParts/AdminViewPowerSupply.java
@@ -0,0 +1,6 @@
+package com.buildacomputer.AdminParts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminViewPowerSupply extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/buildacomputer/AdminViewUsers.java
@@ -0,0 +1,6 @@
+package com.buildacomputer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminViewUsers extends AppCompatActivity {
+}
