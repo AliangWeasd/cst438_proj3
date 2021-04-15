d3313945d894a1ad5a579bd75b60073afcff8e30
Tim <tjrodgers49@gmail.com>
2021-04-15 12:41:10 -0700

    Setup Java files for admin pages.

--- app/src/main/java/com/buildacomputer/AdminViewUsers.java
+++ app/src/main/java/com/buildacomputer/AdminViewUsers.java
@@ -1,6 +1,13 @@
 package com.buildacomputer;
 
+import android.content.Context;
+import android.content.Intent;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AdminViewUsers extends AppCompatActivity {
+
+    public static Intent intentFactory(Context context) {
+        return new Intent(context, AdminViewUsers.class);
+    }
 }
