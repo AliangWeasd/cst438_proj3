f502d56cafdfe6fec454c1d010ab545ac3de5fe4
Sbusane <sambusane@yahoo.com>
2021-04-19 23:45:11 -0700

    Changed signup to use email and FireBase Auth and Finished the login functionality.

--- app/src/main/java/com/buildacomputer/FirebaseAdapters/CompUsers.java
+++ app/src/main/java/com/buildacomputer/FirebaseAdapters/CompUsers.java
@@ -4,16 +4,26 @@ public class CompUsers {
     private String name;
     private String username;
     private String password;
+    private String email;
 
 
-    public CompUsers(String name, String username, String password) {
+    public CompUsers(String name, String username, String password,String email) {
         this.name = name;
         this.username = username;
         this.password = password;
+        this.email = email;
 
     }
 
     public CompUsers() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getName() {
--- app/src/main/java/com/buildacomputer/SignupActivity.java
+++ app/src/main/java/com/buildacomputer/SignupActivity.java
@@ -7,12 +7,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.buildacomputer.FirebaseAdapters.CompUsers;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,18 +31,22 @@ public class SignupActivity extends AppCompatActivity {
     private EditText name;
     private EditText username;
     private EditText password;
+    private EditText email;
     private Button submit;
     private String uName;
     private String uUsername;
     private String uPassword;
+    private String uEmail;
     private boolean valid = true;
     private HashMap user;
     private CompUsers user1;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        auth = FirebaseAuth.getInstance();
         wireUpDisplay();
 
     }
@@ -46,6 +55,7 @@ public class SignupActivity extends AppCompatActivity {
         name = findViewById(R.id.editTextTextPersonName);
         username = findViewById(R.id.editTextTextUserName);
         password = findViewById(R.id.editTextTextPassword);
+        email = findViewById(R.id.editTextTextEmail);
         submit = findViewById(R.id.signup_submit_button);
 
         submit.setOnClickListener(new View.OnClickListener() {
@@ -53,37 +63,31 @@ public class SignupActivity extends AppCompatActivity {
             public void onClick(View view) {
                 getTheFields();
                 if(checkTheFields()){
-                    DatabaseReference ref = FirebaseDatabase.getInstance().getReference("compUser");
-                    Query checkUser = ref.orderByChild("ID");
-                    checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            valid = true;
-                            for (DataSnapshot data : snapshot.getChildren()){
-                                user = (HashMap) data.getValue();
-                                if (user.containsValue(uUsername)){
-                                    Toast.makeText(getApplicationContext(),"Username already taken.",Toast.LENGTH_SHORT).show();
-                                    username.setError("username taken");
-                                    valid = false;
-                                    break;
+                    auth.createUserWithEmailAndPassword(uEmail,uPassword)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()){
+                                        user1 = new CompUsers(uName,uUsername,uPassword,uEmail);
+                                        FirebaseDatabase.getInstance().getReference("compUser")
+                                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                                .setValue(user1).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()){
+                                                    Toast.makeText(SignupActivity.this,"User Registered",Toast.LENGTH_SHORT).show();
+                                                    Intent intent = LoginActivity.intentFactory(getApplicationContext());
+                                                    startActivity(intent);
+                                                }else {
+                                                    Toast.makeText(SignupActivity.this,"User registration failed, try again.",Toast.LENGTH_LONG).show();
+                                                }
+                                            }
+                                        });
+                                    }else {
+                                        Toast.makeText(SignupActivity.this,"User registration failed, try a different Email.",Toast.LENGTH_LONG).show();
+                                    }
                                 }
-                            }
-                            if (valid){
-                                user1 = new CompUsers(uName,uUsername,uPassword);
-                                ref.push().setValue(user1);
-                                Toast.makeText(getApplicationContext(),"User Registered",Toast.LENGTH_SHORT).show();
-                                Intent intent = LoginActivity.intentFactory(getApplicationContext());
-                                startActivity(intent);
-                            }
-
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-
-                        }
-                    });
-
+                            });
 
                 } }
         });
@@ -93,15 +97,34 @@ public class SignupActivity extends AppCompatActivity {
         uName = name.getText().toString();
         uUsername = username.getText().toString();
         uPassword = password.getText().toString();
+        uEmail = email.getText().toString();
     }
 
     private boolean checkTheFields() {
-        return (nameValid()&&usernameValid()&&validPass());
+        return (nameValid()&&usernameValid()&&validPass()&&validEmail());
+    }
+
+    private boolean validEmail() {
+        if (TextUtils.isEmpty(uEmail)){
+            email.setError("Email is required.");
+            return false;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(uEmail).matches()){
+            email.setError("Please Enter valid email");
+            return false;
+        }
+        email.setError(null);
+        return true;
+
     }
 
     private boolean validPass() {
         if (TextUtils.isEmpty(uPassword)){
             password.setError("Password Cannot be Empty");
+            return false;
+        }
+        if (uPassword.length()<6){
+            password.setError("Password must be greater than 6 characters");
             return false;
         }
         password.setError(null);
